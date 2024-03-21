cf0e44de23de330bb37f2b9b8e908ff7ca7d855e
ximenes <thiago@ximenes.dev>
2024-03-21 19:09:04 -0300

    feat(sheet): process sheet and return mmr, churnRate, activeSubscribers and cancelledSubscribers per month

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  FileTypeValidator,
+  HttpStatus,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Express } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/upload-sheet')
+  @UseInterceptors(FileInterceptor('file'))
+  async getSheet(
+    @UploadedFile(
+      new ParseFilePipe({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        validators: [
+          new FileTypeValidator({
+            fileType: 'sheet|csv',
+          }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    return this.appService.processFile(file);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,91 @@
 import { Injectable } from '@nestjs/common';
+import * as csvParser from 'csv-parser';
+import * as xlsx from 'xlsx';
+import { Subscriber } from './types/subscriber.type';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly csvMimeType = 'text/csv';
+  private readonly xlsxMimeType =
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
+
+  async processFile(file: Express.Multer.File) {
+    const processes: Record<
+      string,
+      (file: Express.Multer.File) => Promise<Subscriber[]>
+    > = {
+      [this.csvMimeType]: this.processCsv,
+      [this.xlsxMimeType]: this.processXlsx,
+    };
+
+    const process = processes[file.mimetype];
+
+    const subscribers = await process(file);
+
+    return this.calculateMetrics(subscribers);
   }
+
+  private processCsv = async (
+    file: Express.Multer.File,
+  ): Promise<Subscriber[]> => {
+    const subscribers: Subscriber[] = [];
+
+    csvParser()
+      .on('data', (row) => {
+        row['data início'] = new Date(row['data início']);
+        row['data status'] = new Date(row['data status']);
+        subscribers.push(row);
+      })
+      .write(file.buffer);
+
+    return subscribers;
+  };
+
+  private processXlsx = async (
+    file: Express.Multer.File,
+  ): Promise<Subscriber[]> => {
+    const workbook = xlsx.read(file.buffer);
+    const sheet = workbook.Sheets[workbook.SheetNames[0]];
+    const subscribers: Subscriber[] = xlsx.utils.sheet_to_json(sheet, {
+      raw: false,
+    });
+
+    subscribers.forEach((subscriber) => {
+      subscriber['data início'] = new Date(subscriber['data início']);
+      subscriber['data status'] = new Date(subscriber['data status']);
+    });
+
+    return subscribers;
+  };
+
+  private calculateMetrics = (subscribers: Subscriber[]) => {
+    const metricsByMonth = {};
+
+    subscribers.forEach((subscriber) => {
+      const month = subscriber['data início'].getMonth();
+
+      if (!metricsByMonth[month]) {
+        metricsByMonth[month] = {
+          mrr: 0,
+          churnRate: 0,
+          activeSubscribers: 0,
+          cancelledSubscribers: 0,
+        };
+      }
+
+      if (subscriber.status === 'Ativa') {
+        metricsByMonth[month].mrr += Number(subscriber.valor);
+        metricsByMonth[month].activeSubscribers++;
+      } else if (subscriber.status === 'Cancelada') {
+        metricsByMonth[month].cancelledSubscribers++;
+      }
+
+      metricsByMonth[month].churnRate =
+        metricsByMonth[month].cancelledSubscribers /
+        (metricsByMonth[month].activeSubscribers +
+          metricsByMonth[month].cancelledSubscribers);
+    });
+
+    return metricsByMonth;
+  };
 }
