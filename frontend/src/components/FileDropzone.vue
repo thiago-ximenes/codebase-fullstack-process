b5d71ca8d61a0b9886dbb5612121538e2e3204bf
ximenes <thiago@ximenes.dev>
2024-03-21 09:33:02 -0300

    refactor(home): turn FileDropzone.vue on composition API and separate responsibilities. Add error file css to FileDropzone.vue

--- frontend/src/components/FileDropzone.vue
+++ frontend/src/components/FileDropzone.vue
@@ -1,7 +1,10 @@
 <template>
   <div
       :class="`
-      border-dashed border-2 p-6 rounded-lg cursor-pointer transition-all ease-in-out duration-300
+      border-dashed border-2 p-6 rounded-lg cursor-pointer transition-all ease-in-out duration-300 w-full text-center min-h-40 flex justify-center items-center text-2xl
+
+      ${error ? 'border-red-500 bg-red-100 text-red-800' : 'border-gray-300'}
+
       ${
         isDraggingOver ?
         'border-gray-400 bg-gray-100 text-gray-800' :
@@ -13,71 +16,75 @@
       @drop="handleDrop"
       @click="openFileSelector"
   >
-    <input type="file" ref="fileInput" @change="handleFileSelect" style="display: none" />
+    <input type="file" ref="fileInput" @change="handleFileSelect" style="display: none"/>
     <slot></slot>
-    <div>{{ message }}</div>
+    <div>{{ state.message }}</div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { defineProps, reactive, ref, watchEffect, withDefaults } from 'vue';
 
-export default defineComponent({
-  name: 'FileDropzone',
-  data() {
-    return {
-      message: 'Arraste e solte ou clique para carregar um arquivo',
-      dragOverCount: 0,
-    };
-  },
-  computed: {
-    isDraggingOver() {
-      return this.dragOverCount > 0;
-    },
-  },
-  methods: {
-    dragEnter() {
-      this.dragOverCount++;
-    },
-    dragLeave() {
-      this.dragOverCount--;
-    },
-    handleDrop(e: DragEvent) {
-      e.preventDefault();
+type Props = {
+  message?: string;
+  handleFiles: (files: FileList, setMessage: (message: string) => void) => void;
+};
 
-      this.dragOverCount = 0;
-      const files = e.dataTransfer?.files;
-
-      if (!files?.length) {
-        return;
-      }
-
-      this.handleFiles(files);
-    },
-    openFileSelector() {
-      (this.$refs.fileInput as HTMLInputElement).click();
-    },
-    handleFileSelect(e: Event) {
-      const files = (e.target as HTMLInputElement).files;
-      if (!files?.length) return;
-
-      this.handleFiles(files);
-    },
-    handleFiles(files: FileList) {
-      this.message = 'Carregando...';
-
-      const validExtensionsRegex = new RegExp(/\.(csv|xlsx)$/i);
-
-      const isValidFile = validExtensionsRegex.test(files.item(0)?.name || '');
-
-      if (!isValidFile) {
-        this.message = 'Formato de arquivo inválido. Apenas .csv e .xlsx são aceitos.';
-        return;
-      }
-
-      console.log(files);
-      this.message = 'Arquivo carregado com sucesso!';
-    },
-  },
+const { message, handleFiles } = withDefaults(defineProps<Props>(), {
+  message: 'Arraste e solte um arquivo aqui ou clique para selecionar',
 });
+
+
+const state = reactive({
+  message,
+  dragOverCount: 0,
+});
+
+const error = ref(false);
+
+const isDraggingOver = ref(state.dragOverCount > 0);
+
+const setMessage = (message: string, isError = false) => {
+  state.message = message;
+  error.value = isError;
+};
+
+watchEffect(() => {
+  isDraggingOver.value = state.dragOverCount > 0;
+});
+
+const dragEnter = () => {
+  state.dragOverCount++;
+};
+
+const dragLeave = () => {
+  state.dragOverCount--;
+};
+
+const handleDrop = (e: DragEvent) => {
+  e.preventDefault();
+
+  state.dragOverCount = 0;
+  const files = e.dataTransfer?.files;
+
+  if (!files?.length) {
+    return;
+  }
+
+  handleFiles(files, setMessage);
+};
+
+const fileInput = ref<HTMLInputElement | null>(null);
+
+const openFileSelector = () => {
+  fileInput.value?.click();
+};
+
+const handleFileSelect = (e: Event) => {
+  const files = (e.target as HTMLInputElement).files;
+  if (!files?.length) return;
+
+  handleFiles(files, setMessage);
+};
+
 </script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
 import FileDropzone from '../components/FileDropzone.vue' ;
+
+const handleFiles = (files: FileList, setMessage: (message: string, error?: boolean) => void) => {
+  setMessage('Carregando...');
+
+  const validExtensionsRegex = new RegExp(/\.(csv|xlsx)$/i);
+
+  const isValidFile = validExtensionsRegex.test(files.item(0)?.name || '');
+
+  if (!isValidFile) {
+    setMessage('Formato de arquivo inválido. Apenas .csv e .xlsx são aceitos.', true);
+    return;
+  }
+
+  console.log(files);
+  setMessage('Arquivo carregado com sucesso!');
+};
 </script>
 
 <template>
-  <main class="flex flex-col items-center justify-center h-full">
+  <main class="flex flex-col items-center justify-center h-full p-6">
     <h1 class="text-5xl text-center text-white mb-6">Upload de planilhas</h1>
-    <FileDropzone />
+    <FileDropzone :handle-files="handleFiles"/>
   </main>
 </template>
